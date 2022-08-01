681f14af0dce873379eab6ab4ac6f49e3c24157c
chandrashekaranaik122 <110366329+chandrashekaranaik122@users.noreply.github.com>
2022-08-01 22:16:08 +0530

    Add files via upload

---
+++ student file (1).py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+
+
+# In[5]:
+
+
+import os
+
+
+# In[6]:
+
+
+os.getcwd()
+
+
+# In[7]:
+
+
+import pandas as pd
+
+
+# In[8]:
+
+
+pd.read_csv('student.tsv')
+
+
+# In[ ]:
+
+
+
+
