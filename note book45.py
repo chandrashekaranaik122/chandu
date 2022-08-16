639eb7505bf3f1fe34dbd2965c19fd8483b68744
chandrashekaranaik122 <110366329+chandrashekaranaik122@users.noreply.github.com>
2022-08-16 17:10:13 +0530

    Add files via upload

---
+++ note book45.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+df= pd.read_csv("https://raw.githubusercontent.com/shivam229/dsrscientist-DSData/master/Pokemon.csv")
+
+
+# In[12]:
+
+
+df
+
+
+# In[13]:
+
+
+df.head(49)
+
+
+# In[14]:
+
+
+df.tail(45)
+
+
+# In[ ]:
+
+
+
+
